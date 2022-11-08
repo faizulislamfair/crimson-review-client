58ff40dc23743d126ad189a226f4f1f516804802
faizulislamfair <faizulislamfair@gmail.com>
2022-11-08 22:37:37 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Footer.css';
+import { FaInstagram } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+
 
 const Footer = () => {
     return (
-        <div>
+        <footer className="container-fluid bg-dark text-white text-center mt-5 py-5">
 
-        </div>
+            <h3>Fair Online Delivery Service Review</h3>
+
+            <p>Dhaka, Bangladesh</p>
+
+            <p>Privacy Policy | Terms of use</p>
+
+            <div className="social-icons">
+                <FaFacebook />
+                <FaInstagram />
+                <FaLinkedin />
+            </div>
+
+
+        </footer>
     );
 };
 
