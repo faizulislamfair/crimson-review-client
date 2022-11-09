4d8970f83c668ea6dbc7aa137b8930fbc9bd1bb9
faizulislamfair <faizulislamfair@gmail.com>
2022-11-09 15:26:48 +0600

    details page added

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import './ServiceDetails.css'
 
 const ServiceDetails = () => {
-    return (
-        <div>
 
+    const service = useLoaderData();
+    const { name, image, price, description } = service;
+
+    return (
+        <div className='text-center mt-5 detail p-5'>
+            <h2><b>Name:</b> {name}</h2>
+            <img src={image} alt="" />
+            <h3><b>Price:</b> {price}</h3>
+            <h3><b>Detailed Description:</b> {description}</h3>
         </div>
     );
 };
