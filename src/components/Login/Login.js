44f007aee163a77197439339a31721390aeeb882
faizulislamfair <faizulislamfair@gmail.com>
2022-12-16 19:41:58 +0600

    get jwt on client

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,11 +30,40 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 const user = result.user;
-                console.log(user);
                 form.reset();
-                navigate(from, { replace: true });
+                setError('');
+
+
+                const currentUser = {
+                    email: user.email
+                }
+
+                console.log(currentUser);
+
+
+                // get jwt token
+
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        localStorage.setItem('crimson-token', data.token);
+                    })
+
+
+                // navigate(from, { replace: true });
             })
-            .catch(error => console.error(error));
+            .catch(error => {
+                console.error(error)
+                setError(error.message);
+            }
+            );
 
     }
 
